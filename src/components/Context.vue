bce853e87626ad65e73d248a5cd7923389739071
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-08-30 23:07:01 +0200

    completed the logic for api and data handling

--- src/components/Context.vue
+++ src/components/Context.vue
@@ -20,24 +20,52 @@ export default {
   },
   methods: {
     navigation: function(item) {
-      console.log("navigation", item.name);
+      this.callForData(item.url);
+    },
+    async callForData(url) {
+      const response = await fetch(url);
+      const data = await response.json();
+      this.results = data.results;
     }
   },
   async created() {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=7e12b66ea24703faef4d9209d109eb56&language=en-US&page=1"
-    );
-    const data = await response.json();
-    this.results = data.results;
+    let url = null;
+    this.items.forEach(element => {
+      if (element.active) url = element.url;
+    });
+    this.callForData(url);
   },
   data() {
+    const baseUrl = "https://api.themoviedb.org/3/movie/";
+    const apik = "api_key=7e12b66ea24703faef4d9209d109eb56";
+    const tailUrl = "&language=en-US&page=1";
     return {
       results: null,
       items: [
-        { name: "popular", active: true, id: 0 },
-        { name: "top rated", active: false, id: 1 },
-        { name: "upcoming", active: false, id: 2 },
-        { name: "now playing", active: false, id: 3 }
+        {
+          name: "popular",
+          active: true,
+          id: 0,
+          url: baseUrl + "popular?" + apik + tailUrl
+        },
+        {
+          name: "top rated",
+          active: false,
+          id: 1,
+          url: baseUrl + "top_rated?" + apik + tailUrl
+        },
+        {
+          name: "upcoming",
+          active: false,
+          id: 2,
+          url: baseUrl + "upcoming?" + apik + tailUrl
+        },
+        {
+          name: "now playing",
+          active: false,
+          id: 3,
+          url: baseUrl + "now_playing?" + apik + tailUrl
+        }
       ]
     };
   }
