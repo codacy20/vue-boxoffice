59ea21ac451a4f6a328222e635189a3f072c6a44
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-08-31 21:17:32 +0200

    scroll behaviour

--- src/components/Context.vue
+++ src/components/Context.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <Navigation @onChange="navigation" v-bind:items="items"/>
-    <template v-for="(item,index) in results">
-      <Tile v-bind:item="item" :key="index"/>
-    </template>
+    <div class="wrapper-tiles">
+      <div class="arrow">
+        <span class="material-icons" v-on:click="scroll(0)" v-if="id>0">keyboard_arrow_left</span>
+      </div>
+      <div class="tiles" id="scroller">
+        <Tile v-for="(item,index) in results" v-bind:item="item" :key="index" v-bind:id="index"/>
+      </div>
+      <div class="arrow">
+        <span class="material-icons" v-on:click="scroll(1)">keyboard_arrow_right</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +34,19 @@ export default {
       const response = await fetch(url);
       const data = await response.json();
       this.results = data.results;
+    },
+    scroll(input) {
+      if (input === 0 && this.$data.id > 0) {
+        this.$data.id -= 3;
+      } else if (input === 1) {
+        this.$data.id += 3;
+      } else {
+        return;
+      }
+      let myElement = document.getElementById(this.$data.id);
+      console.log("leftPos", this.$data.id);
+      let leftPos = myElement.offsetLeft - 120;
+      document.getElementById("scroller").scrollLeft = leftPos;
     }
   },
   async created() {
@@ -41,6 +62,7 @@ export default {
     const tailUrl = "&language=en-US&page=1";
     return {
       results: null,
+      id: 0,
       items: [
         {
           name: "popular",
@@ -74,4 +96,41 @@ export default {
 
 
 <style>
+.tiles {
+  display: flex;
+  width: 90vw;
+  max-width: 865px;
+  overflow: scroll;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+}
+.wrapper-tiles {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 90px;
+}
+
+.wrapper-tiles .material-icons {
+  font-size: 40px;
+  color: #747069;
+  cursor: pointer;
+  /* display: none; */
+}
+
+.arrow {
+  width: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tiles {
+  scrollbar-width: none; /* For Firefox */
+  -ms-overflow-style: none; /* For Internet Explorer and Edge */
+}
+
+.tiles::-webkit-scrollbar {
+  width: 0px; /* For Chrome, Safari, and Opera */
+}
 </style>
